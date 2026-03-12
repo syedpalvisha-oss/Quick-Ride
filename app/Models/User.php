afa85a9d166dc265a397f9d0f2e372f17a9514f6
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2026-03-12 17:57:33 +0700

    add soft deletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,6 +15,7 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
     use HasApiTokens;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
