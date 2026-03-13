1139011d2c2fa5427c80270ebf95d4c075866c0f
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2026-03-13 15:15:51 +0700

    Create Currency.php

---
+++ app/Enums/Currency.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum Currency: string
+{
+    case Eur = 'EUR';
+    case Idr = 'IDR';
+    case Myr = 'MYR';
+    case Php = 'PHP';
+    case Sgd = 'SGD';
+    case Thb = 'THB';
+    case Usd = 'USD';
+    case Vnd = 'VND';
+}
